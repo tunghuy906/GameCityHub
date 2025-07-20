bb7f76773d27b12013293a6c958c8bd83df2224e
Tung Huy <tunghuy906@gmail.com>
2025-07-20 15:30:01 +0700

    Clean start: reset commit history

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	private Rigidbody2D rb;
+	private SpriteRenderer spriteRenderer;
+	private Animator animator;
+
+	private float moveInput;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+		
+		moveInput = Input.GetAxisRaw("Horizontal");
+
+		
+		rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+		
+		if (moveInput > 0)
+		{
+			spriteRenderer.flipX = false;
+		}
+		else if (moveInput < 0)
+		{
+			spriteRenderer.flipX = true;
+		}
+
+		
+		bool isMoving = moveInput != 0;
+		animator.SetBool("isMoving", isMoving);
+	}
+}
---
+++ Assets/Script/PlayerManagers.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class PlayerManagers : MonoBehaviour
+{
+    public GameObject[] playerPrefabs;
+	public CinemachineVirtualCamera VCam;
+	int characterIndex;
+	private void Awake()
+	{
+		characterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+		GameObject player = Instantiate(playerPrefabs[characterIndex]);
+		VCam.Follow = player.transform;
+	}
+}
