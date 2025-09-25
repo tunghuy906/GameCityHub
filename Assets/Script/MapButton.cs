e47816e500a0eebc57d8421b688f349efc66ef0f
DICA <tunghuy906@gmail.com>
2025-09-25 10:10:13 +0700

    Update next map

---
+++ Assets/Script/MapButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MapButton : MonoBehaviour, IPointerClickHandler
+{
+	public int sceneIndex;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log("Clicked: " + gameObject.name);
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
