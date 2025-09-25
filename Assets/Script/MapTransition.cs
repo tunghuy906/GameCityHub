107c7d8309e5d27b1d60c098619890a2fdbfbe07
DICA <tunghuy906@gmail.com>
2025-09-25 12:01:19 +0700

    Update map cat room

---
+++ Assets/Script/MapTransition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapTransition : MonoBehaviour
+{
+	[SerializeField] private int sceneIndexToLoad; // index của scene
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player")) // kiểm tra va chạm với Player
+		{
+			SceneManager.LoadScene(sceneIndexToLoad);
+		}
+	}
+}
