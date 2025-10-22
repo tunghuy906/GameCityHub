90c77559b4d07b8c0caa0112c7700da01827566e
DICA <tunghuy906@gmail.com>
2025-10-22 20:03:55 +0700

    Update 2048 game

---
+++ Assets/Script/BangBangShootting/LifeTimeDestroyper.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTimeDestroyper : MonoBehaviour
+{
+    public float Time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, Time);   
+    }
+}
---
+++ Assets/Script/BangBangShootting/PlayerBBST.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBBST : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	public float rollBoost = 2f;
+	private float rollTime;
+	public float RollTime;
+	bool rollOnce = false;
+
+	Rigidbody2D rb;
+	Animator animator;
+
+	public SpriteRenderer characterSR;
+
+	public Vector3 moveInput;
+
+	private void Start()
+	{
+		animator = GetComponentInChildren<Animator>();
+	}
+
+	private void Update()
+	{
+		moveInput.x = Input.GetAxis("Horizontal");
+		moveInput.y = Input.GetAxis("Vertical");
+		transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+		animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+		if(Input.GetKeyDown(KeyCode.Space) && rollTime <= 0)
+		{
+			animator.SetBool("Roll", true);
+			moveSpeed += rollBoost;
+			rollTime = RollTime;
+			rollOnce = true;
+		}
+		if(rollTime <= 0 && rollOnce == true)
+		{
+			animator.SetBool("Roll", false);
+			moveSpeed -= rollBoost;
+			rollOnce = false;
+		}
+		else
+		{
+			rollTime -= Time.deltaTime;
+		}
+		if(moveInput.x != 0)
+		{
+			if(moveInput.x > 0 )
+			{
+				characterSR.transform.localScale = new Vector3(1, 1, 0);
+			}
+			else
+			{
+				characterSR.transform.localScale = new Vector3(-1, 1, 0);
+			}
+		}
+	}
+}
